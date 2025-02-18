359f779ee695f0299801f212ac79879bd0c5aab3
OopsOverflow <houssemmahmoud18@gmail.com>
2025-02-18 14:35:04 +0100

    feat: do some cleanup

--- src/windows/input/input.cpp
+++
@@ -1,6 +0,0 @@
-#include "windows/input/input.hpp"
-#include <windows.h>
-
-bool Input::isCtrlF1Pressed() const {
-    return (GetAsyncKeyState(VK_CONTROL) & 0x8000) && (GetAsyncKeyState(VK_F1) & 0x8000);
-}
